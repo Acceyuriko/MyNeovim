2c00c4185c1363dabd2c3704910089d339cf3290
Acceyuriko <Acceyuriko@gmail.com>
2022-05-16 16:28:30 +0800

    feat: add find_all command

--- init.lua
+++ init.lua
@@ -458,8 +458,16 @@ require('telescope').setup({
 require('telescope').load_extension('projects')
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('ui-select')
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope git_files<cr>', opts);
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files({ hidden=true, file_ignore_patterns={ "%.git[/\\\\]" } })<cr>', opts);
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts);
+vim.api.nvim_set_keymap('n', '<leader>fa',
+  '<cmd>'
+  .. 'lua require("telescope.builtin").find_files({'
+  .. 'hidden=true,'
+  .. 'no_ignore=true,'
+  .. 'file_ignore_patterns={ "%.git[/\\\\]" },'
+  .. '})<cr>',
+  opts
+);
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts);
 vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope live_grep<cr>', opts);
 vim.api.nvim_set_keymap('n', '<leader>fd', '<cmd>Telescope diagnostics bufnr=0<cr>', opts);
